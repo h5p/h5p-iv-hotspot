4f224a3f62d4384a5c866989ffae5dbb54f2d3d2
Paal Joergensen <pal.jorgensen@gmail.com>
2016-08-11 11:36:19 +0200

    Prototype

---
+++ scripts/iv-hotspot.js
@@ -0,0 +1,59 @@
+H5P.IVHotspot = (function ($) {
+
+  /**
+   * Create a new Go To Question!
+   *
+   * @class H5P.IVHotspot
+   * @extends H5P.EventDispatcher
+   * @param {Object} parameters
+   */
+  function IVHotspot(parameters) {
+    var self = this;
+
+    parameters = $.extend(true, {
+      destination: {
+        type: 'timecode',
+        time: '0:00'
+      },
+      visuals: {
+        shape: 'circular',
+        backgroundColor: 'rgba(255, 255, 255, 0.6)'
+      },
+      texts: {}
+    }, parameters);
+
+    /**
+     * Attach the hotspot to the given container.
+     *
+     * @param {H5P.jQuery} $container
+     */
+    self.attach = function ($container) {
+      $container.addClass('h5p-ivhotspot').css({
+        backgroundColor: parameters.visuals.backgroundColor
+      }).addClass(parameters.visuals.shape);
+
+      if (parameters.destination.type === 'url') {
+        var link = new H5P.Link({
+          linkWidget: parameters.destination.url
+        });
+
+        link.attach($container);
+
+        if (parameters.texts.ariaLabel) {
+          $container.find('a').attr('aria-label', parameters.texts.ariaLabel);
+        }
+      }
+      else {
+        var $a = $('<a>', {
+          'aria-label': parameters.texts.ariaLabel
+        }).on('click', function () {
+          self.trigger('goto', parameters.destination.time);
+        });
+        $container.html($a);
+      }
+    };
+  }
+
+  return IVHotspot;
+
+})(H5P.jQuery);
