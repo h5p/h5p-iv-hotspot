f9c5b09fd58a5a806790dd0146280a9c96e83b0d
Frode Petterson <frode.andre.petterson@gmail.com>
2016-09-19 14:41:47 +0200

    Removed automatich blinking in editors
    HFJ-2172

--- scripts/iv-hotspot.js
+++ scripts/iv-hotspot.js
@@ -56,7 +56,7 @@ H5P.IVHotspot = (function ($) {
 
       $a.css({cursor: parameters.visuals.pointerCursor ? 'pointer' : 'default'});
 
-      if (parameters.visuals.animation || window.H5PEditor !== undefined) {
+      if (parameters.visuals.animation) {
         $container.append($('<div>', {
           'class': 'blinking-hotspot'
         }));
